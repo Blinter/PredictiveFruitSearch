203e9d8c8002c159746d22280a9ee0377c9aa223
Blinter <90986012+Blinter@users.noreply.github.com>
2024-04-14 20:10:59 -0400

    Add documentation and fix text search input style adjust

--- script.js
+++ script.js
@@ -4,18 +4,32 @@ const extended = document.querySelector('#extended');
 const suggestions = document.querySelector('div .suggestions');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
-
+/**
+ * Search - Returns the results found based on a string provided
+ * @param {string} str A string that is provided to search the fruit table
+ * @return {Array} A sorted array based on options defined by the user.
+ */
 function search(str) {
 	if (str.length === 0)
 		return [];
 	return sortRelevance(str.toLowerCase(), extended.checked ? undefined : 7);
 }
-
+/**
+ * A callback function when an option is changed or input text is modified.
+ * @param {Event} e Event Handler
+ * @return {void}
+ */
 function searchHandler(e) {
 	suggestions.innerHTML = "";
 	relevance.checked ? showSuggestionsAndScore(search(input.value)) : showSuggestions(search(input.value))
 }
 
+/**
+ * Show Suggestions without scores
+ * Adds HTML elements to the DOM when results are provided
+ * @param {Array} results an Array containing the search results
+ * @return {void}
+ */
 function showSuggestions(results) {
 	results.forEach(v => {
 		const ele1 = document.createElement("span");
@@ -35,7 +49,12 @@ function showSuggestions(results) {
 		suggestions.append(ele1);
 	});
 }
-
+/**
+ * Show Suggestions with scores
+ * Adds HTML elements to the DOM when results are provided
+ * @param {Array} results an Array containing both the search results and scores
+ * @return {void}
+ */
 function showSuggestionsAndScore(results) {
 	const maximum = results.reduce((a, v) => Math.max(a, v[1]), -Infinity);
 	results.forEach((v, i) => {
@@ -59,11 +78,25 @@ function showSuggestionsAndScore(results) {
 	});
 }
 
+/**
+ * Use Suggestion
+ * Event handler is used when user clicks on a search result.
+ * @param {Event} e Event Handler which is based on user's change in search selection.
+ * @return {void}
+ */
 function useSuggestion(e) {
 	input.value = e.target.getAttribute("data-label");
 	suggestions.innerHTML = "";
+	input.removeAttribute("class");
+	input.removeAttribute("data-previous");
 }
 
+/**
+ * Sort Relevance - Main function used to generate search results and score.
+ * @param {String} str Search Term
+ * @param {Number} [resultsAmount] Amount of results based on search or unlimited if not set
+ * @return {Array} Based on user form settings in DOM, return a sorted array with a specific number of results.
+ */
 function sortRelevance(str, resultsAmount = 0) {
 	if (resultsAmount === 0)
 		resultsAmount = undefined;
@@ -100,5 +133,3 @@ relevance.addEventListener("change", searchHandler);
 extended.addEventListener("change", searchHandler);
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
-
-
